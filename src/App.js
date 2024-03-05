9f021f35623a7dd51c5d8c6a4f31b0b3f66d771b
cr0c0 <hyde69ciel@gmail.com>
2024-03-05 16:12:48 +0900

    #240305 Cleanup function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function byfn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byfn; // Cleanup function. 컴포넌트가 destroy될 때 실행되는 함수
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  // useEffect : 함수가 특정 state가 변화할 때만 실행되도록
-  // 파라미터 1 : 함수 / 파라미터 2 : state 변수 (비어있으면 처음 한 번만 실행함)
-  useEffect(() => {
-    console.log("I run only once");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' & 'counter' changes");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
     </div>
   );
 }
